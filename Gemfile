d93f84c64765604df57f9b36ab18c2a0a918f249
Christian Danielsen <ckdanielsen@gmail.com>
2018-04-18 23:19:57 -0700

    initial commit; add basic test

---
+++ Gemfile
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+# Added at 2018-04-18 21:42:43 -0700 by dev:
+gem "faraday", "~> 0.14.0"
+
+# Added at 2018-04-18 21:49:36 -0700 by dev:
+gem "rspec", "~> 3.7"
+
+# Added at 2018-04-18 22:24:07 -0700 by dev:
+gem "nori", "~> 2.6"
+
+# Added at 2018-04-18 22:27:05 -0700 by dev:
+gem "nokogiri", "~> 1.8"
+
+# Added at 2018-04-18 23:11:41 -0700 by dev:
+gem "dotenv", "~> 2.2"
---
+++ spec/test_api.spec.rb
@@ -0,0 +1,37 @@
+require 'faraday'
+require 'nori'
+
+RSpec.describe 'ZillowApi' do
+  describe "The DeepSearch endpoint" do
+    it "responds with a 200 for a valid request" do
+      conn = Faraday.new(:url => 'https://www.zillow.com/')
+      response = conn.get('webservice/GetDeepSearchResults.htm') do |req|
+        req.params['zws-id'] = ENV['ZILLOW_KEY']
+        req.params['address'] = '5305+NE+40th+Ave'
+        req.params['citystatezip'] = 'Portland+OR+97211'
+      end
+
+      # Meta
+      expect(response.success?).to eq(true)
+      expect(response.status).to eq(200)
+
+      # Raw response converted from xml => hash
+      hashedResponse = Nori.new.parse(response.body)
+      # Property information
+      result = hashedResponse['SearchResults:searchresults']['response']['results']['result']
+      
+      expectedBedrooms = '2'
+      actualBedrooms = result['bedrooms']
+      expect(actualBedrooms).to eq(expectedBedrooms)
+      
+      expectedBathrooms = '1.0'
+      actualBathrooms = result['bathrooms']
+      expect(actualBathrooms).to eq(expectedBathrooms)
+
+      expectedYearBuilt = '1949'
+      actualYearBuilt = result['yearBuilt']
+      expect(actualYearBuilt).to eq(expectedYearBuilt)
+
+    end
+  end
+end
